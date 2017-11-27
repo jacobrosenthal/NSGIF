fcdd32bb43a2ae60a7c57a96f7295e5d9afaf884
Jacob Rosenthal <jakerosenthal@gmail.com>
2017-11-26 22:41:50 -0700

    carthage framework scheme

---
+++ DemoProject/NSGIF/NSGIF.h
@@ -0,0 +1,19 @@
+//
+//  NSGIF.h
+//  NSGIF
+//
+//  Created by Jacob Rosenthal on 11/26/17.
+//  Copyright © 2017 Sebastian Dobrincu. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for NSGIF.
+FOUNDATION_EXPORT double NSGIFVersionNumber;
+
+//! Project version string for NSGIF.
+FOUNDATION_EXPORT const unsigned char NSGIFVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSGIF/PublicHeader.h>
+
+#import <NSGIF/NSGIF.h>
